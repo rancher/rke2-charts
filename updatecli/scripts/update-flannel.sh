f6ddca1a309bc7af794e74a4dc1bebb0565aa6f3
Roberto Bonafiglia <roberto.bonafiglia@suse.com>
2025-01-20 17:04:32 +0100

    Fixed update script for Flannel and added automation for cni plugins
    Signed-off-by: Roberto Bonafiglia <roberto.bonafiglia@suse.com>

--- updatecli/scripts/update-flannel.sh
+++ updatecli/scripts/update-flannel.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -eu
+new_package=false
+if [ -n "$CNI_PLUGINS_VERSION" ]; then
+	current_cni_plugins_version=$(sed -nr 's/^\+    tag: ('v[0-9]+.[0-9]+.[0-9]+')/\1/p' packages/rke2-flannel/generated-changes/patch/values.yaml.patch  | tail -1)
+	if [ "$current_cni_plugins_version" != "$CNI_PLUGINS_VERSION" ]; then
+		echo "Updating CNI plugin version to $CNI_PLUGINS_VERSION"
+		sed -i "s/$current_cni_plugins_version/$CNI_PLUGINS_VERSION/g" packages/rke2-flannel/generated-changes/patch/values.yaml.patch
+		package_version=$(yq '.packageVersion' packages/rke2-flannel/package.yaml)
+		new_version=$(printf "%02d" $(($package_version + 1)))
+		yq -i ".packageVersion = $new_version" packages/rke2-flannel/package.yaml
+		new_package=true
+	fi
+fi
 if [ -n "$FLANNEL_VERSION" ]; then
 	app_version=$(echo "$FLANNEL_VERSION" | grep -Eo 'v[0-9]+.[0-9]+.[0-9]+')
 	current_flannel_version=$(sed -nr 's/^\+    tag: ('v[0-9]+.[0-9]+.[0-9]+')/\1/p' packages/rke2-flannel/generated-changes/patch/values.yaml.patch  | head -1)
@@ -7,16 +19,34 @@ if [ -n "$FLANNEL_VERSION" ]; then
 	if [ "$current_flannel_version" != "$FLANNEL_VERSION" ]; then
 		echo "Updating Flannel chart to $FLANNEL_VERSION"
 		if [ "$app_version" != "$current_app_version" ]; then
+			mkdir workdir
+			wget -P workdir/ https://github.com/flannel-io/flannel/releases/download/$app_version/flannel.tgz
+			tar --directory=workdir -xf workdir/flannel.tgz flannel/values.yaml
+			mv workdir/flannel/values.yaml workdir/flannel/values_new.yaml
+			rm workdir/flannel.tgz
+			wget -P workdir/ https://github.com/flannel-io/flannel/releases/download/$current_app_version/flannel.tgz
+			tar --directory=workdir -xf workdir/flannel.tgz flannel/values.yaml
+			current_flannel_plugins_version=$(yq '.flannel.image_cni.tag' workdir/flannel/values.yaml)
+			new_flannel_plugins_version=$(yq '.flannel.image_cni.tag' workdir/flannel/values_new.yaml)
+			current_netpol_version=$(yq '.netpol.image.tag' workdir/flannel/values.yaml)
+			new_netpol_version=$(yq '.netpol.image.tag' workdir/flannel/values_new.yaml)
 			sed -i "s/ version: .*/ version: $app_version/g" packages/rke2-flannel/generated-changes/patch/Chart.yaml.patch
-			sed -i "s/-    tag: $current_app_version/-    tag: $app_version/g" packages/rke2-flannel/generated-changes/patch/values.yaml.patch
+			sed -i ":a;N;\$!ba;s/-    repository: docker.io\\/flannel\\/flannel\\n-    tag: $current_app_version/-    repository: docker.io\\/flannel\\/flannel\\
+-    tag: $app_version/g" packages/rke2-flannel/generated-changes/patch/values.yaml.patch
 			sed -i "s/+    tag: $current_flannel_version/+    tag: $FLANNEL_VERSION/g" packages/rke2-flannel/generated-changes/patch/values.yaml.patch
+			sed -i "s/-    tag: $current_flannel_plugins_version/-    tag: $new_flannel_plugins_version/g" packages/rke2-flannel/generated-changes/patch/values.yaml.patch
+			sed -i ":a;N;\$!ba;s/-    repository: registry.k8s.io\/networking\/kube-network-policies\\n-    tag: $current_netpol_version/-    repository: registry.k8s.io\/networking\/kube-network-policies\\
+-    tag: $new_netpol_version/g" packages/rke2-flannel/generated-changes/patch/values.yaml.patch
 			yq -i ".url = \"https://github.com/flannel-io/flannel/releases/download/$app_version/flannel.tgz\" |
 				.packageVersion = 00" packages/rke2-flannel/package.yaml
+			rm -fr workdir
 		else
 			sed -i "s/+    tag: $current_flannel_version/+    tag: $FLANNEL_VERSION/g" packages/rke2-flannel/generated-changes/patch/values.yaml.patch
-			package_version=$(yq '.packageVersion' packages/rke2-flannel/package.yaml)
-			new_version=$(printf "%02d" $(($package_version + 1)))
-			yq -i ".packageVersion = $new_version" packages/rke2-flannel/package.yaml
+			if [ "$new_package" = false ]; then
+				package_version=$(yq '.packageVersion' packages/rke2-flannel/package.yaml)
+				new_version=$(printf "%02d" $(($package_version + 1)))
+				yq -i ".packageVersion = $new_version" packages/rke2-flannel/package.yaml
+			fi
 		fi
 		GOCACHE='/home/runner/.cache/go-build' GOPATH='/home/runner/go' PACKAGE='rke2-flannel' make prepare
 		find packages/rke2-flannel/charts -name '*.orig' -delete
