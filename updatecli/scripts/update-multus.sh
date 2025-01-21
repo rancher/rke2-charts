517867dcc557fe4702bdc4a0c8e4cfe0180cebf8
Roberto Bonafiglia <roberto.bonafiglia@suse.com>
2025-01-21 16:22:18 +0100

    Added automatic update for cni plugins on multus chart
    Signed-off-by: Roberto Bonafiglia <roberto.bonafiglia@suse.com>

--- updatecli/scripts/update-multus.sh
+++ updatecli/scripts/update-multus.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 set -eu
+new_package=false
+if [ -n "$CNI_PLUGINS_VERSION" ]; then
+	current_cni_plugins_version=$(yq '.cniplugins.image.tag' packages/rke2-multus/charts/values.yaml)
+	if [ "$current_cni_plugins_version" != "$CNI_PLUGINS_VERSION" ]; then
+		echo "Updating CNI plugin version to $CNI_PLUGINS_VERSION"
+		sed -i "s/$current_cni_plugins_version/$CNI_PLUGINS_VERSION/g" packages/rke2-multus/charts/values.yaml
+		package_version=$(yq '.packageVersion' packages/rke2-multus/package.yaml)
+		new_version=$(printf "%02d" $(($package_version + 1)))
+		yq -i ".packageVersion = $new_version" packages/rke2-multus/package.yaml
+		new_package=true
+	fi
+fi
 if [ -n "$MULTUS_VERSION" ]; then
 	app_version=$(echo "$MULTUS_VERSION" | grep -Eo '^v*[0-9]+.[0-9]+.[0-9]+' | tr -d 'v')
 	current_multus_version=$(yq '.image.tag' packages/rke2-multus/charts/values.yaml)
@@ -14,9 +26,11 @@ if [ -n "$MULTUS_VERSION" ]; then
 			yq -i ".packageVersion = 00" packages/rke2-multus/package.yaml
 		else
 			sed -i "s/  tag: $current_multus_version/  tag: $MULTUS_VERSION/g" packages/rke2-multus/charts/values.yaml
-			package_version=$(yq '.packageVersion' packages/rke2-multus/package.yaml)
-			new_version=$(printf "%02d" $(($package_version + 1)))
-			yq -i ".packageVersion = $new_version" packages/rke2-multus/package.yaml
+			if [ "$new_package" = false ]; then
+				package_version=$(yq '.packageVersion' packages/rke2-multus/package.yaml)
+				new_version=$(printf "%02d" $(($package_version + 1)))
+				yq -i ".packageVersion = $new_version" packages/rke2-multus/package.yaml
+			fi
 		fi
 	fi
 fi
