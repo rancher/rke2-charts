ad2b90019a73bc00313e478c50b1899f9e4fc0fb
Michael Fritch <mfritch@suse.com>
2025-07-30 19:51:18 -0600

    updatecli: add failed workflow runs to an existing issue
    Add a comment for failed workflow runs to the existing github issue
    
    Signed-off-by: Michael Fritch <mfritch@suse.com>

--- updatecli/scripts/create-issue.sh
+++ updatecli/scripts/create-issue.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ISSUE_BODY="Url of the failed run: ${UPDATECLI_GITHUB_WORKFLOW_URL}"
+ISSUE_BODY="Failed workflow run: ${UPDATECLI_GITHUB_WORKFLOW_URL}"
 
 GITHUB_APP="rancher-issues-manager"
 GITHUB_REPOSITORY="rancher/rke2"
@@ -10,22 +10,26 @@ report-error() {
     trap - EXIT INT
 
     if [[ $exit_code != 0 ]]; then
-        issues=$(
+        issue=$(
             gh issue list \
                 --app ${GITHUB_APP} \
                 --repo ${GITHUB_REPOSITORY} \
                 --state "open" \
                 --search "${ISSUE_TITLE}" \
-                --json number --jq ".[].number" | wc -l
+                --json number --jq ".[].number" | sort -run | head -1
         )
-        if [[ $issues = 0 ]]; then
-            echo "Creating issue for: ${ISSUE_TITLE}"
+        if [[ -z "$issue" ]]; then
+            echo "Creating issue for: '${ISSUE_TITLE}'"
             gh issue create \
                 --repo ${GITHUB_REPOSITORY} \
                 --title "${ISSUE_TITLE}" \
                 --body "${ISSUE_BODY}"
         else
-            echo "Issue already exists for: ${ISSUE_TITLE}"
+            echo "Issue ${issue} already exists for: '${ISSUE_TITLE}'"
+            gh issue comment \
+                ${issue} \
+                --repo ${GITHUB_REPOSITORY} \
+                --body "${ISSUE_BODY}"
         fi
     fi
 
