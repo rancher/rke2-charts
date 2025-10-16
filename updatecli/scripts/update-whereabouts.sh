ef67a9ca6e4825496b0389297b5b53bbefc07b32
Thomas Ferrandiz <thomas.ferrandiz@suse.com>
2025-10-16 11:25:33 +0000

    Use sed instead of yq in updatecli whereabouts and multus script

--- updatecli/scripts/update-whereabouts.sh
+++ updatecli/scripts/update-whereabouts.sh
@@ -6,6 +6,9 @@ source $(dirname $0)/create-issue.sh
 ISSUE_TITLE="Updatecli failed for whereabouts ${WHEREABOUTS_VERSION}"
 trap report-error EXIT INT
 
+# Note: we use sed instead of yq to update the packageVersion field
+# since yq insists on injecting the tag !!int in front of the value
+# when the number is 08 or 09
 if [ -n "$WHEREABOUTS_VERSION" ]; then
 	app_version=$(echo "$WHEREABOUTS_VERSION" | grep -Eo '^v*[0-9]+.[0-9]+.[0-9]+' | tr -d 'v')
 	current_whereabouts_version=$(yq '.image.tag' packages/rke2-whereabouts/charts/values.yaml)
@@ -16,18 +19,18 @@ if [ -n "$WHEREABOUTS_VERSION" ]; then
 			sed -i "s/version: .*/version: $app_version/g" packages/rke2-whereabouts/charts/Chart.yaml
 			sed -i "s/appVersion: .*/appVersion: $app_version/g" packages/rke2-whereabouts/charts/Chart.yaml
 			sed -i "s/  tag: $current_whereabouts_version/  tag: $WHEREABOUTS_VERSION/g" packages/rke2-whereabouts/charts/values.yaml
-			yq -i ".packageVersion = 00" packages/rke2-whereabouts/package.yaml
+			sed -i "s/packageVersion:.*/packageVersion: 00 /g" packages/rke2-whereabouts/package.yaml
 			multus_package_version=$(yq '.packageVersion' packages/rke2-multus/package.yaml)
 			multus_new_version=$(printf "%02d" $(($multus_package_version + 1)))
-			yq -i ".packageVersion = $multus_new_version" packages/rke2-multus/package.yaml
+			sed -i "s/packageVersion:.*/packageVersion: $multus_new_version /g" packages/rke2-multus/package.yaml
 		else
 			sed -i "s/  tag: $current_whereabouts_version/  tag: $WHEREABOUTS_VERSION/g" packages/rke2-whereabouts/charts/values.yaml
 			package_version=$(yq '.packageVersion' packages/rke2-whereabouts/package.yaml)
 			new_version=$(printf "%02d" $(($package_version + 1)))
-			yq -i ".packageVersion = $new_version" packages/rke2-whereabouts/package.yaml
+			sed -i "s/packageVersion:.*/packageVersion: $new_version /g" packages/rke2-whereabouts/package.yaml
 			multus_package_version=$(yq '.packageVersion' packages/rke2-multus/package.yaml)
 			multus_new_version=$(printf "%02d" $(($multus_package_version + 1)))
-			yq -i ".packageVersion = $multus_new_version" packages/rke2-multus/package.yaml
+			sed -i "s/packageVersion:.*/packageVersion: $multus_new_version /g" packages/rke2-multus/package.yaml
 		fi
 	fi
 fi
